f357122c0f1efdba7f258ba18a1a364b5ac2a75b
Peter Bangert <pbangert@psi.de>
2018-10-11 15:19:19 +0200

    adding new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+import { CommentsComponent } from './comments/comments.component';
+import { SimilarVideosComponent } from './similar-videos/similar-videos.component';
+import { VideoInfoComponent } from './video-info/video-info.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { VideoBrowserComponent } from './video-browser/video-browser.component';
+import { BrowserComponent } from './browser/browser.component';
+import { VideoplayerComponent } from './videoplayer/videoplayer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideoPlayerComponent,
+    CommentsComponent,
+    SimilarVideosComponent,
+    VideoInfoComponent,
+    HomeComponent,
+    NavBarComponent,
+    SideBarComponent,
+    VideoBrowserComponent,
+    BrowserComponent,
+    VideoplayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
